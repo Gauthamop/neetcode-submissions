f73d758ffe367cafa96b88abcdb74d72ab92d090
Gautham Poornachandran <157595973+Gauthamop@users.noreply.github.com>
2026-08-09 09:27:53 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def is_palindrome(left,right):
+            while left<=right:  #cannot cross , but max can be equal 
+                if s[left]==s[right]:
+                    left+=1
+                    right-=1
+                else:
+                    return False
+            return True
+            
+
+        left=0 #starts at the start
+        right=len(s)-1  #starts at the end
+        while left<right:
+            if s[left]!=s[right]:
+                return (is_palindrome(left+1,right) or is_palindrome(left,right-1))
+
+            left+=1
+            right-=1
+
+        return True
+        
+        
+
+
+
+
+
+
+
+
+
+        
