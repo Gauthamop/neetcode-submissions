0cf7f0db29f4e1e08a9638127b9d3f200e39b0d1
Gautham Poornachandran <157595973+Gauthamop@users.noreply.github.com>
2026-08-10 12:07:37 +0530

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.py
@@ -0,0 +1,27 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left=0 #starts at the start of the array
+        right=len(numbers)-1 #starts at the end
+
+        while left<right: #as mentioned they cannot be equal to each other
+            if numbers[left]+numbers[right]<target:
+                left+=1
+            elif numbers[left]+numbers[right]>target:
+                right-=1
+
+            else:
+                return [left+1,right+1]   #1-indexed
+        
+
+
+        
+
+
+
+
+
+
+
+
+    
+        
