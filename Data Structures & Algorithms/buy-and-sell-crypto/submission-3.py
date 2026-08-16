acdb7815a2d5ce1fd2eb7fcdec725e8c8953373b
Gautham Poornachandran <157595973+Gauthamop@users.noreply.github.com>
2026-08-16 12:04:24 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #best time to buy and sell stock 
+        #sliding window approach
+        #variable type sliding window
+        left=0
+        n=len(prices)
+        profit=0 #current profit=0
+        max_profit=0
+
+        for right in range(left+1,n):
+            profit=0
+            if prices[right]>prices[left]:
+                profit=prices[right]-prices[left]
+                max_profit=max(max_profit,profit)
+            
+        
+
+            else:
+                left=right #the next least buying price
+
+
+        return max_profit
+        
+
+
+
+
+
+        
