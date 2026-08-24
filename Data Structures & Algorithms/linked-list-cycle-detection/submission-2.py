0536abee0508e6d33080e80e3f573dc76582b3ab
Gautham Poornachandran <157595973+Gauthamop@users.noreply.github.com>
2026-08-24 13:16:59 +0530

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        #linked list cycle detection , #cycle means when any node is linked to a previous node , we can use a hashset to keep track of nodes we have already visited 
+        #so we need a index value aswell, but that is not provided to us
+        #can only be solved using linked list approach 
+        hashset=set() #we can keep track of visited nodes
+
+        curr=head #the starting value
+
+
+        while (curr!=None):
+    
+            if curr not in hashset:
+                hashset.add(curr)  #this adds the node itself , ADD the node itself, dont add the value 
+                curr=curr.next
+
+            elif curr in hashset:
+                return True
+
+        
+        return False
+
+            
+
+
